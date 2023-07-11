4a4ba2e379da8a5fbb44e456b6721983d546c16f
MAM <muzammilmohd538@gmail.com>
2023-07-11 20:04:45 +0530

    mobile

--- islamictutor.js
+++ islamictutor.js
@@ -3,20 +3,20 @@ const headerLinks = document.querySelector(".header_links");
 const headerForm = document.querySelector(".header_form");
 const searchIcon = document.querySelector("#searchBtn");
 
-toggler(menuIcon, headerLinks, "bi-list", "bi-x-lg")
-toggler(searchIcon, headerForm, "bi-search", "bi-x-lg")
+toggler(menuIcon, headerLinks, "bi-list", "bi-x-lg");
+toggler(searchIcon, headerForm, "bi-search", "bi-x-lg");
 
 function toggler(btnEle, divEle, pClass, nClass) {
-  if(btnEle && divEle){
-    btnEle.addEventListener("click", ()=>{
-      divEle.classList.toggle("active")
-      if(btnEle.classList.contains(pClass)){
-        btnEle.classList.remove(pClass)
-        btnEle.classList.add(nClass)
-      }else if(btnEle.classList.contains(nClass)){
-        btnEle.classList.remove(nClass)
-        btnEle.classList.add(pClass)
+  if (btnEle && divEle) {
+    btnEle.addEventListener("click", () => {
+      divEle.classList.toggle("active");
+      if (btnEle.classList.contains(pClass)) {
+        btnEle.classList.remove(pClass);
+        btnEle.classList.add(nClass);
+      } else if (btnEle.classList.contains(nClass)) {
+        btnEle.classList.remove(nClass);
+        btnEle.classList.add(pClass);
       }
-    })
+    });
   }
 }
