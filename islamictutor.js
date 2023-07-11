03d6a796488e0c461ba6bdfc6850d04b3eb57ce5
MAM <muzammilmohd538@gmail.com>
2023-07-11 19:53:21 +0530

    main

--- islamictutor.js
+++ islamictutor.js
@@ -1,6 +1,22 @@
 const menuIcon = document.querySelector("#menuIcon");
 const headerLinks = document.querySelector(".header_links");
+const headerForm = document.querySelector(".header_form");
+const searchIcon = document.querySelector("#searchBtn");
 
-menuIcon.addEventListener("click", ()=>{
-  headerLinks.classList.toggle("active")
-})
+toggler(menuIcon, headerLinks, "bi-list", "bi-x-lg")
+toggler(searchIcon, headerForm, "bi-search", "bi-x-lg")
+
+function toggler(btnEle, divEle, pClass, nClass) {
+  if(btnEle && divEle){
+    btnEle.addEventListener("click", ()=>{
+      divEle.classList.toggle("active")
+      if(btnEle.classList.contains(pClass)){
+        btnEle.classList.remove(pClass)
+        btnEle.classList.add(nClass)
+      }else if(btnEle.classList.contains(nClass)){
+        btnEle.classList.remove(nClass)
+        btnEle.classList.add(pClass)
+      }
+    })
+  }
+}
