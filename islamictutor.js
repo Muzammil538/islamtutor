9ddfe15ce14ed616fc47fa886ebb10523934c6e4
MAM <muzammilmohd538@gmail.com>
2023-07-11 21:15:54 +0530

    main finish

--- islamictutor.js
+++ islamictutor.js
@@ -2,9 +2,22 @@ const menuIcon = document.querySelector("#menuIcon");
 const headerLinks = document.querySelector(".header_links");
 const headerForm = document.querySelector(".header_form");
 const searchIcon = document.querySelector("#searchBtn");
+const searchFormBtn = document.querySelector("#searchFormBtn");
+const headerSearch = document.querySelector("#headerSearch");
+const cardTitles = document.querySelectorAll(".card-title");
+
+
+searchFormBtn.addEventListener("submit", findTab)
 
 toggler(menuIcon, headerLinks, "bi-list", "bi-x-lg");
 toggler(searchIcon, headerForm, "bi-search", "bi-x-lg");
+
+function findTab(e) {
+  e.preventDefault();
+  let inputVal = headerSearch.value;
+  
+}
+
 
 function toggler(btnEle, divEle, pClass, nClass) {
   if (btnEle && divEle) {
