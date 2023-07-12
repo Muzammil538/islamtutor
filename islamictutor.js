167abeccceeeac7d08de8cb27f1fcf906d40566c
MAM <muzammilmohd538@gmail.com>
2023-07-12 20:56:24 +0530

    dua

--- islamictutor.js
+++ islamictutor.js
@@ -6,17 +6,27 @@ const searchFormBtn = document.querySelector("#searchFormBtn");
 const headerSearch = document.querySelector("#headerSearch");
 const cardTitles = document.querySelectorAll(".card-title");
 
-
-searchFormBtn.addEventListener("submit", findTab)
+headerSearch.addEventListener("change", (e)=>{
+  e.preventDefault();
+  let val = headerSearch.value;
+  if(val.length >= 3){
+    searchFormBtn.addEventListener("click", ()=>{
+      for (let i = 0; i < cardTitles.length; i++) {
+        let ary = [];
+        // let element = cardTitles[i].innerHTML;
+        ary.push(cardTitles[i].innerHTML)
+        console.log(ary);
+        
+      }
+    })
+  }else{
+    headerSearch.value = ""
+  }
+})
 
 toggler(menuIcon, headerLinks, "bi-list", "bi-x-lg");
 toggler(searchIcon, headerForm, "bi-search", "bi-x-lg");
 
-function findTab(e) {
-  e.preventDefault();
-  let inputVal = headerSearch.value;
-  
-}
 
 
 function toggler(btnEle, divEle, pClass, nClass) {
